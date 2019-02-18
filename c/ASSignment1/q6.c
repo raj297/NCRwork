ae9727a643f7976a1b6f237c33640d3f9ef13aa2
raj297 <sanganabatlaraj297@gmail.com>
2019-02-18 21:15:30 +0530

    added

---
+++ c/ASSignment1/q6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	unsigned int num;
+	int a[5], i,j;
+	printf("Enter a number :");
+	scanf_s("%d", &num);
+	if (num < 32)
+	{
+		for (i = 0;i < 5;i++)
+		{
+			a[i] = num % 2;
+			num = num/2;
+		}
+		i = 4;
+		while (a[i] == 0)
+			i--;
+		for (j = i;j >= 0;j--)
+		{
+			printf("%d", a[j]);
+		}
+
+	}
+	else
+		printf("Num is greater than 31");
+	getch();
+}
