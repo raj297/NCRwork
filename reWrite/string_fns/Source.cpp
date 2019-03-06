a819f5530b5992651e285ff93eb614758afd3818
raj297 <sanganabatlaraj297@gmail.com>
2019-03-06 20:46:52 +0530

    update

--- reWrite/string_fns/Source.cpp
+++ reWrite/string_fns/Source.cpp
@@ -1,64 +1,74 @@
 #include<iostream>
+#define lenmax 10
 using namespace std;
 class allStringFns
 {
 public:
+	int l = lenmax;//used during dynaminc memeory allocation yo string
 	int stringlen(char *p);
 	void stringrev(char *str);
 	void stringcopy(char *s1, char *s2);
     void stringconcat(char *s1, char *s2);
    int stringcomp(char *s1, char *s2);
+   //wrapper to rev
    void rev()
    {
-	   char *str1; int l;
-	   cout << "enter string 1 lenght" << endl;
-	   cin >> l;
+	   char *str1; 
+	   //check here....
+	  // cout << "enter string 1 lenght" << endl;
+	 //  cin >> l;
 	   str1 = new char[l];
 	   cout << "enter string 1" << endl;
 	   cin >> str1;
 	   stringrev(str1);
    }
+   //wrapper to compare
    void comp()
    {
-	   char *str1, *str2; int l;
-	   cout << "enter string 1 lenght" << endl;
-	   cin >> l;
+	   char *str1, *str2;
+	  // cout << "enter string 1 lenght" << endl;
+	  // cin >> l;
 	   str1 = new char[l];
 	   cout << "enter string 1" << endl;
 	   cin >> str1;
-	   cout << "enter string 2 lenght" << endl;
-	   cin >> l;
+	   //cout << "enter string 2 lenght" << endl;
+	   //cin >> l;
 	   str2 = new char[l];
 	   cout << "enter string 12" << endl;
 	   cin >> str2;
 	   stringcomp(str1, str2);
    }
+   //wrapper to concate
    void concat()
    {
-	   char *str1, *str2; int l;
-	   cout << "enter string 1 lenght" << endl;
-	   cin >> l;
+	   char *str1, *str2;
+	//   cout << "enter string 1 lenght" << endl;
+//cin >> l;
 	   str1 = new char[l];
 	   cout << "enter string 1" << endl;
 	   cin >> str1;
-	   cout << "enter string 2 lenght" << endl;
-	   cin >> l;
+	  // cout << "enter string 2 lenght" << endl;
+	 //  cin >> l;
 	   str2 = new char[l];
 	   cout << "enter string 12" << endl;
 	   cin >> str2;
 	   stringconcat(str1, str2);
    }
+   //wrapper to copy
    void copy()
    {
-	   char *str1, *str2; int l;
-	   cout << "enter string 1 lenght" << endl;
-	   cin >> l;
+	   char *str1, *str2; 
+	  // cout << "enter string 1 lenght" << endl;
+	 //  cin >> l;
 	   str1 = new char[l];
 	   cout << "enter string 1" << endl;
-	   str2 = new char[l];
+	   cin >> str1;
+	   str2 = new char[stringlen(str1)];
 	   stringcopy(str2, str1);
+	   cout << str1;
    }
 };
+//implementation of funtions
 int allStringFns:: stringlen(char *p) {
 	int count = 0;
 	for (; *p != '\0'; p++)
@@ -77,7 +87,12 @@ void allStringFns:: stringrev(char *str) {
 	cout << endl << "reversed to" << str;
 }
 void allStringFns::stringcopy(char *s1, char *s2) {
-	while ((*s1++ = *s2++) != '\0');
+
+	while (*s2 != '\0')
+	{
+		*s1++ = *s2++;
+	}
+	cout << endl<<"copy sucessful copied  ::";
 }
 void  allStringFns::stringconcat(char *s1, char *s2) {
 
@@ -87,45 +102,69 @@ void  allStringFns::stringconcat(char *s1, char *s2) {
 		s1[i] = s2[j];
 	}
 	s1[i] = '\0';
-	cout <<endl<< "concatinated to   " << s1;
+	cout <<endl<< "concatinated to " << s1;
 }
 int allStringFns:: stringcomp(char *s1, char *s2) {
-	while (*s1 == *s2) {
+	int ret;
+	while (*s1 == *s2)
+	{
 		if (*s1 == '\0')
-			return *s1 - *s2;
-		s1++;
+		{
+			ret= *s1 - *s2;
+			break;
+		}s1++;
 		s2++;
 	}
-	return *s1 - *s2;
+	ret=*s1 - *s2;
+	if (ret == 0)
+		cout <<endl<< "yes matched";
+	else
+		cout << endl << "Not matched";
+	return ret;
 }
 
 int main()
 {
 	allStringFns obj;
 	int choice;
-	cout << "enter Your Choice" << endl;
-	cout << "1.String Compare" << endl;
-	cout << "2.String Concat" << endl;
-	cout << "3.String Reverse" << endl;
-	cout << "4.String Copy" << endl;;
-	cin >> choice;
-	switch (choice)
-	{
-	case 1:	
-		obj.comp();
-		break;
+	while (1) {
+		cout <<endl<< "enter Your Choice" << endl;
+		cout << "1.String Compare" << endl;
+		cout << "2.String Concat" << endl;
+		cout << "3.String Reverse" << endl;
+		cout << "4.String Copy" << endl;
+		cout << "5.String len" << endl;
+		cout << "6.Exit" << endl;;
+		cin >> choice;
 
-	case 2:
-		obj.concat();
-		break;
+		switch (choice)
+		{
+		case 1:
+			obj.comp();
+			break;
 
-	case 3: obj.rev();
-		break;
+		case 2:
+			obj.concat();
+			break;
 
-	case 4:obj.copy();
-		break;
-	default:cout << "enter the right choice!!!";
-		break;
+		case 3: obj.rev();
+			break;
+
+		case 4:obj.copy();
+			break;
+
+		case 5:char *str;
+			str = new char[lenmax];
+			cout << endl<<"enter the string"<<endl;
+			cin >> str;
+			cout<<endl<<"length=="<<obj.stringlen(str)<<endl;
+			break;
+
+		case 6:exit(1);
+			break;
+		default:cout << "enter the right choice!!!";
+			break;
+		}
 	}
 	getchar();
 	getchar();
